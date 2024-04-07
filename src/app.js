5f48a8a0eeee9c88e40e893e3ab0463c605bbe26
Rafael Bonaldi <rafael.simoes@supermix.com.br>
2024-04-07 18:33:11 -0300

    Criação de rotas

--- src/app.js
+++ src/app.js
@@ -6,34 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+const indexRoute = require('./routes/index-route')
+const productsRoute = require('./routes/products-route.js')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.1'
-    });
-});
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-const put = router.put('/:id', (req, res, next) => {
-    let id = req.params.id;
-
-    res.status(201).send({
-        ID: id,
-        item: req.body
-    });
-});
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productsRoute);
 
 module.exports = app;
